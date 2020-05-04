d1df2a2dde5b48e67146e505789a71e4f0ca2103
Vlad Florea <vladalexgit@users.noreply.github.com>
2020-05-04 21:48:17 +0300

    can send imu message

--- streamer/ros_node/nemostreamer/src/nemo_publisher.py
+++ streamer/ros_node/nemostreamer/src/nemo_publisher.py
@@ -3,17 +3,19 @@ import socket
 import pickle
 import rospy
 
-from std_msgs.msg import String
+from std_msgs.msg import String, Header
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Quaternion, Vector3
 
-host = '192.168.100.128'  #'192.168.183.130'
+host = '192.168.100.128'  # '192.168.183.130'
 port = 6366
 HEADER_SIZE = 10
 BUFFER_SIZE = 16
 
 
 def publisher():
-
-    pub = rospy.Publisher('nemo_streamer', String, queue_size=10)
+    string_publisher = rospy.Publisher('nemo_streamer', String, queue_size=10)
+    imu_publisher = rospy.Publisher('nemo_streamer', Imu, queue_size=10)
 
     client_socket = socket.socket()
 
@@ -46,12 +48,31 @@ def publisher():
 
             packet = pickle.loads(message)
 
-            ros_msg = String()
-            ros_msg.data = str(packet)
+            # packet was received, preparing to send multiple ROS messages derived from it
 
-            pub.publish(ros_msg)
+            # TEST
+            # ros_msg = String()
+            # ros_msg.data = str(packet)
+            # string_publisher.publish(ros_msg)
+            # rospy.loginfo(str(packet))
 
-            rospy.loginfo(str(packet))
+            nemo_header = Header()  # will be used for all messages which require headers
+            nemo_header.stamp = rospy.Time.now()
+
+            # send IMU message http://docs.ros.org/api/sensor_msgs/html/msg/Imu.html
+
+            imu_msg = Imu(
+                header=nemo_header,
+                orientation=Quaternion(x=0.0, y=0.0, z=0.0, w=1.0),
+                orientation_covariance=[0.99] * 9,
+                angular_velocity=Vector3(x=0.1, y=0.2, z=0.0),
+                angular_velocity_covariance=[0.99] * 9,
+                linear_acceleration=Vector3(x=0.0, y=0.1, z=0.0),
+                linear_acceleration_covariance=[0.99] * 9,
+            )
+
+            imu_publisher.publish(imu_msg)
+            rospy.loginfo(str(imu_msg))
 
         except Exception as e:
             rospy.loginfo(type(e))
