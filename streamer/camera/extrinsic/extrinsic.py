5a57a443781cc2b98a6bbffd76542b7989ac62ee
Vlad Florea <vladalexgit@users.noreply.github.com>
2020-07-15 20:35:26 +0300

    added bilinear interpolation

--- streamer/camera/extrinsic/extrinsic.py
+++ streamer/camera/extrinsic/extrinsic.py
@@ -1,8 +1,7 @@
 import cv2
 import yaml
-from glob import glob
 import numpy as np
-import pickle
+from scipy import interpolate
 
 GRID_SIZE = (7, 10)
 
@@ -27,34 +26,56 @@ for f, meas in calib_samples.items():
         objpoints.append(objp)
 
         corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+        outer_corners = {
+            "top-left": tuple(corners2[0][0]),
+            "top-right": tuple(corners2[GRID_SIZE[1] - 1][0]),
+            "bot-left": tuple(corners2[((GRID_SIZE[0] - 1) * GRID_SIZE[1]) + 0][0]),
+            "bot-right": tuple(corners2[((GRID_SIZE[0] - 1) * GRID_SIZE[1]) + (GRID_SIZE[1] - 1)][0]),
+        }
+
+        meas_draw = img.copy()
+
+        for corner_pos, corner_coord in outer_corners.items():
+
+            int_coords = (int(corner_coord[0]), int(corner_coord[1]))
+
+            cv2.circle(meas_draw, int_coords, 10, (0, 0, 255), -1)
+            cv2.putText(meas_draw, str(meas[corner_pos]), int_coords, cv2.FONT_HERSHEY_SIMPLEX, 8, (255, 0, 0), 5, cv2.LINE_AA)
+
+        small = cv2.resize(meas_draw, (int(meas_draw.shape[1] / 4), int(meas_draw.shape[0] / 4)))
+        cv2.imshow('real_meas', small)
+        # cv2.waitKey(0)
+
+        # TODO bilinear interp
+
+        x, y, z = [], [], []
+
+        for corner_pos, corner_coord in outer_corners.items():
+            x.append(corner_coord[0])
+            y.append(corner_coord[1])
+            z.append(meas[corner_pos])
+
+        distance_func = interpolate.interp2d(x, y, z, kind='linear')
+
+        for i in range(GRID_SIZE[0]):
+            for j in range(GRID_SIZE[1]):
+
+                coords = (corners2[(i * GRID_SIZE[1]) + j][0][0], corners2[(i * GRID_SIZE[1]) + j][0][1])
+                int_coords = tuple(map(int, coords))
+
+                cv2.circle(meas_draw, int_coords, 10, (0, 255, 0), -1)
+                cv2.putText(meas_draw, f"{distance_func(coords[0], coords[1]).item():.2f}", int_coords, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4, cv2.LINE_AA)
+
+        small = cv2.resize(meas_draw, (int(meas_draw.shape[1] / 4), int(meas_draw.shape[0] / 4)))
+        cv2.imshow('interp_meas', small)
+        cv2.waitKey(0)
+
         imgpoints.append(corners2)
 
-        img = cv2.drawChessboardCorners(img, GRID_SIZE, corners2, ret)
-        cv2.imshow('img', img)
-        cv2.imwrite("checkerboard_corners.jpg", img)
-        cv2.waitKey(0)
+        # img = cv2.drawChessboardCorners(img, GRID_SIZE, corners2, ret)
+        # cv2.imshow('img', img)
+        # cv2.imwrite("checkerboard_corners.jpg", img)
+        # cv2.waitKey(0)
     else:
         print("No corners found!")
-
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-calib_data = {'mtx': mtx, 'dist': dist, 'rvecs': rvecs, 'tvecs': tvecs, 'objpoints': objpoints, 'imgpoints': imgpoints} # TODO struct
-
-with open("calib_coefs_cam.pkl", "wb") as f:
-    pickle.dump(calib_data, f)
-
-for f in input_files:
-    image = cv2.imread(f)
-
-    h, w = image.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(calib_data['mtx'], calib_data['dist'], (w, h), 1, (w, h))
-
-    corrected = image.copy()
-
-    # undistort
-    cv2.undistort(image, calib_data['mtx'], calib_data['dist'], corrected, newcameramtx)
-    # crop the image
-    x, y, w, h = roi
-    corrected = corrected[y:y + h, x:x + w]
-
-    cv2.imwrite("calibrated_sample.jpg", corrected)
