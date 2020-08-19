36315adcc04882143a0209c899212ec2b2087e61
Alexandru Sorici <alex.sorici@gmail.com>
2020-08-19 15:01:53 +0300

    Setting correct header information for NemoPublisher ROS Node; Configuring nemo_fusion launch file for robot_localization ROS node that fuses IMU, GPS and CAN speed data

--- streamer/ros_node/nemostreamer/src/nemo_publisher.py
+++ streamer/ros_node/nemostreamer/src/nemo_publisher.py
@@ -28,7 +28,7 @@ def publisher():
 
     string_publisher = rospy.Publisher('nemo_streamer', String, queue_size=settings["queue_size"])
     imu_publisher = rospy.Publisher('nemo_imu', Imu, queue_size=settings["queue_size"])
-    nmea_publisher = rospy.Publisher('nmea_sentence', Sentence, queue_size=settings["queue_size"]) # TODO make sure nobody is publishing to this same topic
+    nmea_publisher = rospy.Publisher('nemo_nmea_sentence', Sentence, queue_size=settings["queue_size"]) # TODO make sure nobody is publishing to this same topic
     speed_publisher = rospy.Publisher('nemo_odom', Odometry, queue_size=settings["queue_size"])
 
 
@@ -74,16 +74,18 @@ def publisher():
             # string_publisher.publish(ros_msg)
             # rospy.loginfo(str(packet))
 
-            nemo_header = Header()  # will be used for all messages which require headers
-            #nemo_header.stamp = rospy.Time.now()  # TODO get from packet timestamp
-            nemo_header.stamp = rospy.Time.from_sec(packet["datetime"])
+            # nemo_header = Header()  # will be used for all messages which require headers
+            # nemo_header.stamp = rospy.Time.now()  # TODO get from packet timestamp
+            # nemo_header.stamp = rospy.Time.from_sec(packet["datetime"])
 
             # send IMU message http://docs.ros.org/api/sensor_msgs/html/msg/Imu.html
             imu_packet = packet["sensor_data"]["imu"]
             rospy.logdebug(imu_packet)
 
+            imu_header = Header(frame_id="imu", stamp=rospy.Time.from_sec(packet["datetime"]))
             imu_msg = Imu(
-                header=nemo_header,
+                # header=nemo_header,
+                header=imu_header,
                 orientation=Quaternion(
                     x=imu_packet["orientation_quaternion"]["x"],
                     y=imu_packet["orientation_quaternion"]["y"],
@@ -103,26 +105,30 @@ def publisher():
             )
 
             imu_publisher.publish(imu_msg)
-            # rospy.loginfo(str(imu_msg))
 
             # send GPS data to nmea_topic_driver of nmea_navsat_driver package
-            for msg_type, msg_str in packet["sensor_data"]["gps"].items():
+            for msg_type, msg_data in packet["sensor_data"]["gps"].items():
 
                 # TODO caching might flood the ROS nmea_driver package, inspect this
+                # nmea_msg = Sentence(
+                #     header=nemo_header, # TODO The header.stamp should correspond to the time that the message was read from the device for accurate time_reference output
+                #     sentence=msg_str,
+                # )
 
-                nmea_msg = Sentence(
-                    header=nemo_header, # TODO The header.stamp should correspond to the time that the message was read from the device for accurate time_reference output
-                    sentence=msg_str,
-                )
-
-                nmea_publisher.publish(nmea_msg)
-                # rospy.loginfo(str(nmea_msg))
+                # NOTE: it may be necessary to convert other message types to GGA type
+                # (as this is required by navsat_transform)
+                if msg_type == "GGA":
+                    gps_header = Header(frame_id="gps", stamp=msg_data["timestamp"])
+                    nmea_msg = Sentence(header=gps_header, sentence=msg_data["msg"])
+                    nmea_publisher.publish(nmea_msg)
 
             # send speed data to nemo_odometry topic
             speed_data = packet["sensor_data"]["speed"]
             mps = speed_data["mps"]
-            odom_msg = Odometry(header=nemo_header)
-            odom_msg.header.frame_id = "odom"
+
+            odom_header = Header(frame_id="odom", stamp=rospy.Time.from_sec(packet["datetime"]))
+            odom_msg = Odometry(header=odom_header)
+            odom_msg.child_frame_id = "base_link"
 
             # pose will not be used, so we are setting it to constant zero
             odom_msg.pose = PoseWithCovariance(pose=Pose(position=Point(0.0, 0.0, 0.0),
@@ -131,9 +137,13 @@ def publisher():
             # for the twist we are currently specifying the speed only on the X axis
             # TODO: the speed value needs to be broken down based on the IMU or GPS values (or difference of values)
             odom_msg.twist = TwistWithCovariance(twist=Twist(linear=Vector3(mps, 0, 0),
-                                                             angular=Vector3(0, 0, 0)))
-            rospy.loginfo(str(odom_msg))
-
+                                                             angular=Vector3(0, 0, 0)),
+                                                 covariance=[0.5, 0.,0., 0.,0.,0., # lin x
+                                                             0.,0.5, 0., 0.,0.,0., # lin y
+                                                             0.,0.,0.5, 0., 0.,0., # lin z
+                                                             0.,0.,0., 0.5, 0.,0., # ang x
+                                                             0.,0.,0., 0.,0.5, 0., # ang y
+                                                             0.,0.,0., 0.,0.,0.5]) # ang z
             speed_publisher.publish(odom_msg)
 
             _end_time = time.time()
