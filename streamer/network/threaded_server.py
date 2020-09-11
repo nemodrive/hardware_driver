67f25d477a108a43f3177c5bbe0660fba2666e7d
Alexandru Sorici <alex.sorici@gmail.com>
2020-09-11 19:05:38 +0300

    Fixing usage of Manager() objects in stream_server

--- streamer/network/threaded_server.py
+++ streamer/network/threaded_server.py
@@ -1,11 +1,31 @@
 import logging
 import zlib
-import socket
+import socket, select
+import multiprocessing
+import queue
 from multiprocessing import Process, Manager, Event, Queue
+from multiprocessing.managers import SyncManager
 from typing import List, Tuple
 from math import log10
 
 import pickle
+
+logger = multiprocessing.get_logger()
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+NEMO_STREAM_MANAGER_KEY = "nemo".encode()
+
+
+class NemoStreamManager(SyncManager):
+    """
+    Implements the manager process that synchronizes the data collection process with the
+    processes serving individual clients (e.g. GUI, recorder, ROS Publisher. It maintains:
+      - the list of client workers (i.e. subscribed processes that deliver messages from the shared memory streamer
+      - the flag that specifies that the shared memory collector process is still working
+    """
+    def __init__(self, address=('0.0.0.0', 8986), authkey=NEMO_STREAM_MANAGER_KEY):
+        super(NemoStreamManager, self).__init__(address=address, authkey=authkey)
 
 
 class ThreadedBroadcastServer:
@@ -14,11 +34,16 @@ class ThreadedBroadcastServer:
     Each client is allocated its own worker process and message Queue for fast operation.
     """
 
-    def __init__(self, host: str, port: int, header_size: int):
+    # def __init__(self, sync_manager_address, host: str, port: int, header_size: int):
+    def __init__(self, collector_running: multiprocessing.Event, stream_workers,
+                 sync_manager_host, sync_manager_port,
+                 host: str, port: int, header_size: int):
         """
         Instantiates the server and starts the worker process which accepts connections on the specified port.
 
         Args:
+            sync_manager: The multiprocessing.Manager object used to sync between the sensor data collector process
+            and the distributor processes that connect to it via sockets
             host (str): Server host address
             port (int): Server port where new connection requests will be sent
             header_size (int): Number of bytes used to represent object size
@@ -28,69 +53,84 @@ class ThreadedBroadcastServer:
         self.port = port
         self.header_size = header_size
 
-        manager = Manager()
+        self._client_acceptor_running = collector_running
+        self._stream_workers = stream_workers
 
-        self._client_workers = manager.list()
-        self._worker_running = manager.Event()
-
-        self._worker_running.set()
+        # create and start the client listener process
+        # self._accept_clients_worker = Process(target=self._accept_clients_thread, args=(self.sync_manager.address,))
         self._accept_clients_worker = Process(target=self._accept_clients_thread,
-                                              args=(self._client_workers, self._worker_running))
+                                              args=(collector_running, stream_workers))
         self._accept_clients_worker.start()
 
-    def _accept_clients_thread(self, client_workers: List, running: Event):
+    def __create_manager(self):
+        return Manager()
+
+    def _accept_clients_thread(self, collector_running, stream_workers):
+    # def _accept_clients_thread(self, sync_manager_address):
         """
         Blueprint method for the thread which listens on the main port and opens a connection for each new client.
 
         Args:
-            client_workers (List): A list shared by the multiprocessing Manager where the new clients will be added
-            running (Event): The process will shut down when this event is cleared
+            # client_workers (List): A list shared by the multiprocessing Manager where the new clients will be added
+            # running (Event): The process will shut down when this event is cleared
+            sync_manager_address: address for the sync_manager Manager from which the run flag and the shared list
+            of clients is obtained
         """
-
+        # create listening socket
         main_socket = socket.socket()
         main_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         main_socket.settimeout(0.2)
 
-        manager = Manager()
-
         try:
             main_socket.bind((self.host, self.port))
         except socket.error as e:
-            logging.error(e)
+            logger.error(e)
 
-        logging.debug('Server started, waiting for clients...')
+        logger.info('Server started, waiting for clients...')
         main_socket.listen(5)
+        main_socket.setblocking(False)
 
-        child_workers = []
+        child_workers: List[Tuple[Process, Queue, Event]] = []
 
-        while running.is_set():
-            try:
+        local_worker_manager = self.__create_manager()
+
+        while collector_running.is_set():
+            readable, writable, err = select.select([main_socket.fileno()], [], [], 1)
+            if readable:
                 client_socket, client_address = main_socket.accept()  # this is why it will not join
-                logging.debug('Connection request from: %s:%d' % (client_address[0], client_address[1]))
 
-                is_running = manager.Event()
-                is_running.set()
-                sender_queue = manager.Queue()
+                logger.info('Connection request from: %s:%d' % (client_address[0], client_address[1]))
+
+                child_worker_running = local_worker_manager.Event()
+                child_worker_running.set()
+                sender_queue = local_worker_manager.Queue()
+
                 sender_worker = Process(target=self._send_to_client_thread,
-                                        args=(sender_queue, client_socket, client_address, is_running))
+                                        args=[sender_queue, client_socket, client_address, child_worker_running])
                 sender_worker.start()
 
-                client_workers.append((sender_queue, is_running))
+                stream_workers.append((sender_queue, child_worker_running))
+                child_workers.append((sender_worker, sender_queue, child_worker_running))
 
-                child_workers.append((sender_worker, is_running))
-            except socket.timeout:
-                continue
+        logger.info("Stopping client threads...")
 
-        logging.debug("Stopping client threads...")
+        for _, _, child_worker_running in child_workers:
+            child_worker_running.clear()
 
-        for _, is_running in child_workers:
-            is_running.clear()
+        # for _, sender_queue, child_worker_running in child_workers:
+        #     sender_queue.join_thread()
 
-        for w, is_running in child_workers:
+        for w, _, child_worker_running in child_workers:
             w.join()
 
+        # local_worker_manager.shutdown()
+
+        # when the collector_running flag is false, it means no data can be forwarded any more
+        # so we can
         main_socket.close()
+
+        logger.info("Client acceptor shutting down!")
 
     def _send_to_client_thread(self, packet_queue: Queue, destination_socket: socket.socket,
                                client_address: Tuple[str, int], is_running: Event):
@@ -103,20 +143,24 @@ class ThreadedBroadcastServer:
             client_address Tuple[str, int]: Client host and port
             is_running: The process will shut down when this event is cleared
         """
-
         while is_running.is_set():
-
-            crt_packet = packet_queue.get()  # TODO analyze queue sizes during operation
-
             try:
+                crt_packet = packet_queue.get(timeout=1)  # TODO analyze queue sizes during operation
+
                 destination_socket.sendall(crt_packet)
+                packet_queue.task_done()
+            except queue.Empty as e:
+                logger.debug("No data within one second; Retrying.")
             except (ConnectionAbortedError, ConnectionResetError, BrokenPipeError):
-                logging.debug("Client at %s:%d has aborted connection, removing from queue" % client_address)
+                logger.info("Client at %s:%d has aborted connection, removing from queue" % client_address)
                 is_running.clear()
             except Exception as e:
-                logging.error(type(e))
-                logging.error(e)
+                logger.error(type(e))
+                logger.error(e)
                 is_running.clear()
+
+        # packet_queue.close()
+        logger.info("Stream worker for client %s has terminated" % str(destination_socket.getsockname()))
 
     def object_to_bytes(self, obj):
         """
@@ -152,7 +196,10 @@ class ThreadedBroadcastServer:
 
         disconnected_clients = []
 
-        for i, worker_info in enumerate(self._client_workers):
+        stream_workers = self._stream_workers
+        print(stream_workers)
+
+        for i, worker_info in enumerate(stream_workers):
             # send it to all clients
 
             sender_queue, is_running = worker_info
@@ -165,13 +212,13 @@ class ThreadedBroadcastServer:
 
         if len(disconnected_clients) > 0:
             for client_index in sorted(disconnected_clients, reverse=True):
-                del disconnected_clients[client_index]
+                del stream_workers[client_index]
             disconnected_clients.clear()
 
     def close(self):
         """Shuts down the server and clears all used resources"""
-        self._worker_running.clear()
-        logging.debug("waiting for workers to join")
+        self._client_acceptor_running.clear()
+        logger.info("###################### waiting for workers to join")
         self._accept_clients_worker.join()
 
     def __enter__(self):
--- streamer/stream_server.py
+++ streamer/stream_server.py
@@ -3,8 +3,13 @@ import yaml
 
 # from streamer import SharedMemStreamer
 from streamer_simulator import SimulatedStreamer
-from network.threaded_server import ThreadedBroadcastServer
+from network.threaded_server import ThreadedBroadcastServer, NemoStreamManager, NEMO_STREAM_MANAGER_KEY
+import multiprocessing
+import logging
 
+logger = multiprocessing.log_to_stderr()
+logger.setLevel(logging.INFO)
+logger.propagate = False
 
 server_running = True
 
@@ -19,18 +24,34 @@ signal.signal(signal.SIGINT, _handle_signal)
 
 
 def main():
-
+    # Create synchronized sensor stream service
     # streamer = SharedMemStreamer()
     streamer = SimulatedStreamer()
 
+    # read config for network streaming
     with open("config.yaml", "r") as f:
         settings = yaml.load(f, Loader=yaml.SafeLoader)["network_streaming"]
 
-    with ThreadedBroadcastServer(settings["host"], settings["port"], settings["header_size"]) as server:
+    # create collector_running flag and worker_list and register them with the StreamManager class
+
+    # instantiate synchronization Manager for relaying sensor data to different clients
+    stream_sync_manager = NemoStreamManager(address=(settings["sync_manager_host"],
+                                                     settings["sync_manager_port"]),
+                                            authkey=NEMO_STREAM_MANAGER_KEY)
+
+    stream_sync_manager.start()
+    stream_workers = stream_sync_manager.list()
+    collector_running = stream_sync_manager.Event()
+    collector_running.set()
+
+    with ThreadedBroadcastServer(collector_running, stream_workers,
+                                 settings["sync_manager_host"],
+                                 settings["sync_manager_port"],
+                                 settings["host"], settings["port"], settings["header_size"]) as server:
 
         input("Press Enter to begin ...")
 
-        for data_packet in streamer.stream_generator():
+        for data_packet in streamer.stream_generator(time_delayed=True, simulator_delay=None):
 
             if not server_running:
                 break
@@ -40,8 +61,9 @@ def main():
             server.send_object(data_packet)
             print(data_packet)
 
-        server.close()
+    stream_sync_manager.shutdown()
 
+    logger.info("Stopped Stream Server")
 
 
 if __name__ == '__main__':
